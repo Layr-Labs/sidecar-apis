02190443cac1dbcc5aa5fb5549d3365edd0dd94d
Sean McGary <sean@seanmcgary.com>
2024-09-25 09:39:48 -0700

    Import and generate protos

---
+++ gen/api-ts/eigenlayer/sidecar/v1/api.pb.ts
@@ -0,0 +1,104 @@
+/* eslint-disable */
+// @ts-nocheck
+/*
+* This file is a generated Typescript file for GRPC Gateway, DO NOT MODIFY
+*/
+
+import * as fm from "../../../fetch.pb"
+export type GetBlockHeightRequest = {
+}
+
+export type GetBlockHeightResponse = {
+  blockNumber?: string
+  blockHash?: string
+}
+
+export type GetStateRootRequest = {
+  blockNumber?: string
+}
+
+export type GetStateRootResponse = {
+  ethBlockNumber?: string
+  ethBlockHash?: string
+  stateRoot?: string
+}
+
+export type GetRewardsRootRequest = {
+  blockNumber?: string
+}
+
+export type GetRewardsRootResponse = {
+  rewardsRoot?: string
+}
+
+export type GenerateClaimProofRequest = {
+  earnerAddress?: string
+  tokens?: string[]
+}
+
+export type EarnerLeaf = {
+  earner?: string
+  earnerTokenRoot?: string
+}
+
+export type TokenLeaf = {
+  token?: string
+  cumulativeEarnings?: string
+}
+
+export type Proof = {
+  root?: string
+  rootIndex?: number
+  earnerIndex?: number
+  earnerTreeProof?: string
+  earnerLeaf?: EarnerLeaf
+  leafIndices?: number[]
+  tokenTreeProofs?: string[]
+  tokenLeaves?: TokenLeaf[]
+}
+
+export type GenerateClaimProofResponse = {
+  proof?: Proof
+}
+
+export type GetAvailableRewardsRequest = {
+  earnerAddress?: string
+}
+
+export type Reward = {
+  token?: string
+  amount?: string
+}
+
+export type GetAvailableRewardsResponse = {
+  rewards?: Reward[]
+}
+
+export type GetAvailableRewardsTokensRequest = {
+  earnerAddress?: string
+}
+
+export type GetAvailableRewardsTokensResponse = {
+  tokens?: string[]
+}
+
+export class Rpc {
+  static GetBlockHeight(req: GetBlockHeightRequest, initReq?: fm.InitReq): Promise<GetBlockHeightResponse> {
+    return fm.fetchReq<GetBlockHeightRequest, GetBlockHeightResponse>(`/v1/latest-block`, {...initReq, method: "POST", body: JSON.stringify(req, fm.replacer)})
+  }
+  static GetStateRoot(req: GetStateRootRequest, initReq?: fm.InitReq): Promise<GetStateRootResponse> {
+    return fm.fetchReq<GetStateRootRequest, GetStateRootResponse>(`/v1/state-roots/${req["blockNumber"]}`, {...initReq, method: "POST", body: JSON.stringify(req, fm.replacer)})
+  }
+  static GetRewardsRoot(req: GetRewardsRootRequest, initReq?: fm.InitReq): Promise<GetRewardsRootResponse> {
+    return fm.fetchReq<GetRewardsRootRequest, GetRewardsRootResponse>(`/v1/rewards-roots/${req["blockNumber"]}`, {...initReq, method: "POST", body: JSON.stringify(req, fm.replacer)})
+  }
+  static GenerateClaimProof(req: GenerateClaimProofRequest, initReq?: fm.InitReq): Promise<GenerateClaimProofResponse> {
+    return fm.fetchReq<GenerateClaimProofRequest, GenerateClaimProofResponse>(`/v1/claim-proof`, {...initReq, method: "POST", body: JSON.stringify(req, fm.replacer)})
+  }
+  static GetAvailableRewards(req: GetAvailableRewardsRequest, initReq?: fm.InitReq): Promise<GetAvailableRewardsResponse> {
+    return fm.fetchReq<GetAvailableRewardsRequest, GetAvailableRewardsResponse>(`/v1/available-rewards`, {...initReq, method: "POST", body: JSON.stringify(req, fm.replacer)})
+  }
+  static GetAvailableRewardsTokens(req: GetAvailableRewardsTokensRequest, initReq?: fm.InitReq): Promise<GetAvailableRewardsTokensResponse> {
+    return fm.fetchReq<GetAvailableRewardsTokensRequest, GetAvailableRewardsTokensResponse>(`/v1/available-rewards-tokens`, {...initReq, method: "POST", body: JSON.stringify(req, fm.replacer)})
+  }
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Layr-Labs/sidecar-apis
+
+go 1.21.10
